a2b9034ce1f38f878e5a6fe1b7faf59f5e989577
nicolemorais <nicolemoraisatt@gmail.com>
2026-06-21 01:49:49 -0300

    chore: limpa a branch main removendo o monolito antigo (preservado em monolith-archive)

--- src/main/java/br/ifsp/studyhub_api/dto/AlunoResponseDTO.java
+++
@@ -1,9 +0,0 @@
-package br.ifsp.studyhub_api.dto;
-
-import java.util.UUID;
-
-public record AlunoResponseDTO(
-    UUID id,
-    String nome,
-    String email
-) {}
--- src/main/java/br/ifsp/studyhub_api/dto/LoginRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package br.ifsp.studyhub_api.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record LoginRequestDTO(
-        @NotBlank(message = "E-mail ou senha inválidos.") String email,
-
-        @NotBlank(message = "E-mail ou senha inválidos.") String senha
-){}
