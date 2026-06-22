2222b52a01e4c4f6c2a293411f26fc028cf3c030
nicolemorais <nicolemoraisatt@gmail.com>
2026-06-21 23:04:01 -0300

    feat(auth): inicializa o microsserviço de autenticação

---
+++ studyhub-auth-service/studyhub-auth-service/src/main/java/br/com/studyhub/auth/StudyhubAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.studyhub.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyhubAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyhubAuthServiceApplication.class, args);
+	}
+
+}
---
+++ studyhub-auth-service/studyhub-auth-service/src/test/java/br/com/studyhub/auth/StudyhubAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.studyhub.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyhubAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
