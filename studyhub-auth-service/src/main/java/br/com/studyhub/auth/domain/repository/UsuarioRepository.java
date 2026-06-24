3f62f007873e235eb9ffcc7f2ecc97f8593f58b1
nicolemorais <nicolemoraisatt@gmail.com>
2026-06-24 11:52:13 -0300

    build(auth): configura persistência, Dockerfile e propriedades de ambiente

--- studyhub-auth-service/src/main/java/br/com/studyhub/auth/domain/repository/UsuarioRepository.java
+++ studyhub-auth-service/src/main/java/br/com/studyhub/auth/domain/repository/UsuarioRepository.java
@@ -6,10 +6,9 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.studyhub.auth.domain.model.Usuario;
-import br.com.studyhub.auth.interfaces.dto.CadastroRequestDTO;
+
 
 public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+    
     Optional<Usuario> findByEmail(String email);
-
-    void registrarUsuario(CadastroRequestDTO request);
 }
