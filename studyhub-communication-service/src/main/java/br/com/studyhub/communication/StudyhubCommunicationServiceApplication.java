99604ce3e20b543bcc96ffcae42eefb6d0081be1
nicolemorais <nicolemoraisatt@gmail.com>
2026-06-24 14:24:47 -0300

    feat(communication): inicializa o microsserviço de communicação

---
+++ studyhub-communication-service/src/main/java/br/com/studyhub/communication/StudyhubCommunicationServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.studyhub.communication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyhubCommunicationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyhubCommunicationServiceApplication.class, args);
+	}
+
+}
---
+++ studyhub-communication-service/src/test/java/br/com/studyhub/communication/StudyhubCommunicationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.studyhub.communication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyhubCommunicationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
