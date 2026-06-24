68ea55970b6979fcf175a2a955886a0e5cf49b70
nicolemorais <nicolemoraisatt@gmail.com>
2026-06-24 14:16:36 -0300

    feat(classroom): inicializa o microsserviço de sala

---
+++ studyhub-classroom-service/src/main/java/br/com/studyhub/classroom/StudyhubClassroomServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.studyhub.classroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyhubClassroomServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyhubClassroomServiceApplication.class, args);
+	}
+
+}
---
+++ studyhub-classroom-service/src/test/java/br/com/studyhub/classroom/StudyhubClassroomServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.studyhub.classroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyhubClassroomServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
