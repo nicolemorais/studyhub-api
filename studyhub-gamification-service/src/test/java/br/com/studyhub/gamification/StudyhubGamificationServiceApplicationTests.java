80667fd988f1e4aac920c6eedf8a15ee94852b8f
nicolemorais <nicolemoraisatt@gmail.com>
2026-06-24 14:21:46 -0300

    feat(gamification): inicializa o microsserviço de gamificação

---
+++ studyhub-gamification-service/src/test/java/br/com/studyhub/gamification/StudyhubGamificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.studyhub.gamification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyhubGamificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
